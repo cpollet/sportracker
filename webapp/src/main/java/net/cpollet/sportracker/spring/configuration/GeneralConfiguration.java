79c6ca7b2137d4585bdaeac99ad68db24fedf2c4
Christophe Pollet <christophe@pollet.ch>
2014-06-25 22:16:14 +0200

    updated JSON converter to serialize date as string

---
+++ webapp/src/main/java/net/cpollet/sportracker/spring/configuration/GeneralConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Christophe Pollet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.cpollet.sportracker.spring.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+/**
+ * @author Christophe Pollet
+ */
+@Configuration
+public class GeneralConfiguration {
+	@Bean
+	public MappingJackson2HttpMessageConverter jackson2Converter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setObjectMapper(objectMapper());
+		return converter;
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		return objectMapper;
+	}
+}
